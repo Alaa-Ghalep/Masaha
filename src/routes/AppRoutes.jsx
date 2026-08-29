822ac683bf83b0a541da617c91acfb0e8eca3d61
Alaa-Ghalep <alaaabuhamra06@gmail.com>
2026-08-29 15:42:57 +0300

    first commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
